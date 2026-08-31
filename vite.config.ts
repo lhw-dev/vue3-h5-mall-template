1135efda29c9b4b327324518316407986e3dc267
lhw-code <lhw2435@qq.com>
2026-08-31 21:15:45 +0800

    git commit -m "perf: vite打包优化"

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,10 @@ export default defineConfig({
       dirs: 'src/views',
       importMode: 'async',
     }),
-    (Layouts as any)({
+    // @ts-expect-error
+    // upstream vite-plugin-vue-layouts type bug https://github.com/johncampionjr/vite-plugin-vue-layouts/issues/165
+    // vue‑tsc命令行校验正常，仅VSCode project‑reference编辑器TS识别异常
+    Layouts({
       layoutsDirs: 'src/layouts',
       defaultLayout: 'TabLayout',
     }),
@@ -55,7 +58,32 @@ export default defineConfig({
       symbolId: 'icon-[dir]-[name]',
     }),
   ],
-  // 移动端开发服务配置
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+    emptyOutDir: true,
+    // 打包优化
+    minify: 'esbuild',
+    // 拆分大模块，分包输出
+    rollupOptions: {
+      output: {
+        // 分包策略：第三方库单独拆包
+        manualChunks: {
+          vueVendor: ['vue', 'vue-router', 'pinia'],
+          vant: ['vant'],
+          i18n: ['vue-i18n'],
+          msw: ['msw'],
+        },
+        // 静态资源文件名格式化
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      },
+    },
+    // 大于4kb资源转为base64，小图片内联，减少http请求
+    assetsInlineLimit: 4096,
+  },
+  // 服务配置
   server: {
     host: '0.0.0.0',
     port: 9527,
